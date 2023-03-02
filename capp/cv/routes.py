73c248a8dce818a1ee45ec1c5b7243508af3d659
s203645 <90606782+s203645@users.noreply.github.com>
2023-03-02 09:49:03 +0100

    Delete capp directory

--- capp/cv/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, Blueprint
-cv=Blueprint('cv',__name__)
-
-@cv.route("/cv")
-def cv_home():
-    return render_template("cv.html", title="CV")
--- capp/developer/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, Blueprint
-developer=Blueprint('developer',__name__)
-
-@developer.route("/developer")
-def developer_home():
-    return render_template("developer.html", title="Developer")
